188dce8293b51face0483ef41ec254b9a45f6f49
afullhart <58483164+afullhart@users.noreply.github.com>
2021-09-04 12:27:50 -0700

    Update solrad_subdaily_variables.py
    Overhaul with different filtering approach.

--- solrad_subdaily_variables.py
+++ solrad_subdaily_variables.py
@@ -1,18 +1,17 @@
 
+import time
 import ee
 import datetime as dt
-import time
 import math
 now = dt.datetime.now()
 
 #ee.Authenticate()
 ee.Initialize()
 
-
 product_name = 'NASA/GLDAS/V021/NOAH/G025/T3H'
 band_labels = ['SWdown_f_tavg']
-asset = ee.FeatureCollection( 'users/andrewfullhart/PimaSantaCruz_ERA_Grid' )
-start = '2000-01-01'
+asset = 'users/andrewfullhart/PimaSantaCruz_ERA_Grid'
+start = '2000-01-02'
 end = '2020-01-01'
 
 #convert_unit_factor_ = (3.*3600.) / 41840. 
@@ -26,6 +25,7 @@ stations = ee.FeatureCollection( asset )
 #stations = ee.FeatureCollection( station_feats.slice( 0, 3 ) )
 
 stationIDs = ee.List( stations.reduceColumns( ee.Reducer.toList(), ['stationID'] ).get( 'list' ) )
+stations_list_ = stations.toList( 1000000 )
 
 station_groups = []
 tmp = []
@@ -44,9 +44,28 @@ for i in range(stationIDs.size().getInfo()):
 station_groups.append( tmp )            
 station_groups_ = ee.List( station_groups )
 
-def dates_list_to_feats( date ):
-    dct = {'system:time_start': ee.Date( date ).millis()}
-    return ee.Feature( None, dct )
+strs_of_stats_ = ee.List( ['mean', 'stdDev'] )
+number_of_stats_seq_ = ee.List.sequence( 0, strs_of_stats_.length().add( -1 ) )
+months_seq = ee.List.sequence( 1, 12 )
+
+start_date = dt.datetime.strptime( start, '%Y-%M-%d' )
+end_date = dt.datetime.strptime( end, '%Y-%M-%d' )
+diff = abs((end_date - start_date).days)
+
+monthly_nested_dates_ = []
+for i in range(12):
+    mo_dates = []
+    for j in range(diff):
+        date = dt.timedelta( days=j ) + start_date
+        if date.month == i+1:
+            eedate = ee.Date( dt.datetime.strftime( date, '%Y-%m-%d' ) )
+            mo_dates.append( eedate )
+        else:
+            pass
+        
+    monthly_nested_dates_.append( ee.List( mo_dates ) )
+
+monthly_ee_dates_ = ee.List( monthly_nested_dates_ )
 
 def region_array_to_feats( img_vals ):
     val_list = ee.List( img_vals )
@@ -58,88 +77,80 @@ def dict_list_unpacker( dct_obj ):
     dct = ee.Dictionary( dct_obj )
     return dct.values()
 
+def list_unpacker( eelist ):
+    eelist = ee.List( eelist )
+    return eelist.get( 4 )
+
+
 for batch_i in range(batch_ct):
     stationID_batch_list = ee.List( station_groups_.get( batch_i ) )
     batch_filter = ee.Filter.inList( 'stationID', stationID_batch_list )
-    stations_ = stations.filter( batch_filter )
-    
-    def main_funcs_caller( mo ):
-        month_filter = ee.Filter.calendarRange( mo, mo, 'month' )
-        ic = ee.ImageCollection( product_name )                                    \
-                .filterDate( start, end )                                          \
-                .filter( month_filter )                                            \
-                .select( band_labels[0] )
-    
-        def station_funcs( station ):
-            ic_prop_array = ic.getRegion( ee.FeatureCollection( station ), 1000 ).slice( 1 )     
-            raw_data_fc = ee.FeatureCollection( ic_prop_array.map( region_array_to_feats ) )
-    
-            def daily_sorter( day ):
-                start = ee.Date( day.get( 'system:time_start' ) )
-                end = start.advance( 1, 'day' )
-                day_filter = ee.Filter.date( start, end )
-                fc = raw_data_fc.filter( day_filter )
-                total = fc.reduceColumns( ee.Reducer.sum(), ee.List( ['SWin'] ) )
-                acc = ee.Number( total.get( 'sum' ) )#.multiply( convert_unit_factor_ )
-                return ee.Feature( None, {'SWin': acc} )
-    
-            day_dates_fc = dates_fc_.filter( month_filter )
-            day_vals_fc = ee.FeatureCollection( day_dates_fc.map( daily_sorter ) )
-            mean = day_vals_fc.reduceColumns( ee.Reducer.mean(), ['SWin'] )
-            stdDev = day_vals_fc.reduceColumns( ee.Reducer.sampleStdDev(), ['SWin'] )
+    stations_ = stations_list_.filter( batch_filter )
+    stations_n_ = stations_.length()
 
-            dct = {'mean': mean, 'stdDev': stdDev}
-            return ee.Feature( None, dct )
+    def main_funcs_caller( moobj ):
+        mo = ee.Number( moobj )
+        dates = ee.List( monthly_ee_dates_.get( mo.add( -1 ) ) )
 
-        out_fc = ee.FeatureCollection( stations_.map( station_funcs ) )
-        mean = out_fc.reduceColumns( ee.Reducer.toList(), ['mean'] ).get( 'list' )
-        stdDev = out_fc.reduceColumns( ee.Reducer.toList(), ['stdDev'] ).get( 'list' )
-        out_list = ee.List( [] )
-        out_list = out_list.add( ee.List( mean ).map( dict_list_unpacker ) )
-        out_list = out_list.add( ee.List( stdDev ).map( dict_list_unpacker ) )
-        return out_list
+        def date_func( dateobj ): 
+            date = ee.Date( dateobj )
+            ic = ee.ImageCollection( product_name )                                                                                                                                                                             \
+                    .filterDate( date, date.advance( 1, 'day' ) )                 \
+                    .select( band_labels[0] )
 
-    def list_flatten( list_obj ):
-        return ee.List( list_obj ).flatten()
-    
-    def out_list_unpacker( station_id ):
-        station_str = ee.String( station_id )
-        station_i = station_ids_strs_.indexOf( station_str )
-        def stat_features( nested_i ):
-            nested_i = ee.Number( nested_i ).int()
-            nested_i_list = ee.Array( out_list_.get( nested_i ) ).transpose().toList()
-            nested_i_list = nested_i_list.map( list_flatten ).get( station_i )
-            station_stat_str = station_str.cat( '_' ).cat( strs_of_stats_.get( nested_i ) )
-            dct = {'station_ID': station_stat_str,
-                    'statistic': nested_i_list}
-            return ee.Feature( None, dct )
-        fc = ee.FeatureCollection( number_of_stats_seq_.map( stat_features ) )
-        return fc
-    
-    dates = []
-    start_date = ee.Date( start )
-    end_date = ee.Date( end )
-    n = end_date.difference( start_date, 'day' )
-    for i in range(n.getInfo()):
-        date = start_date.advance( ee.Number( i ), 'day' )
-        dates.append( date )
-    dates_ee = ee.List( dates )
-    dates_fc_ = ee.FeatureCollection( dates_ee.map( dates_list_to_feats ) )
-    
-    station_ids = ee.List( stations_.reduceColumns( ee.Reducer.toList(), ['stationID'] ).get( 'list' ) )
-    station_ids_strs_ = ee.List( [str(elem) for elem in station_ids.getInfo()] )
-    
-    months_seq = ee.List.sequence( 1, 12 )
-    
-    strs_of_stats_ = ee.List( ['mean', 'stdDev'] )
-    number_of_stats_seq_ = ee.List.sequence( 0, strs_of_stats_.length().add( -1 ) ) 
-    
+            def station_func( stationobj ):
+                station = ee.Feature( stationobj )
+                raw_data_list = ic.getRegion( station.geometry(), 1000 ).slice( 1 ) 
+                data_list = raw_data_list.map( list_unpacker )
+                total = ee.Number( data_list.reduce( ee.Reducer.sum() ) )
+                return total
+  
+            out_list = stations_.map( station_func )            
+            return out_list
+
+        out_list = dates.map( date_func )
+
+        def stats_func( indexobj ):
+            index = ee.Number( indexobj )
+
+            def column_list_func( listobj ):
+                eeList = ee.List( listobj )
+                eeNum = ee.Number( eeList.get( index ) )
+                return eeNum
+
+            station_data = out_list.map( column_list_func )                
+            mean = ee.Number( station_data.reduce( ee.Reducer.mean() ) )
+            stdDev = ee.Number( station_data.reduce( ee.Reducer.sampleStdDev() ) )
+            return ee.List( [mean, stdDev] )
+
+        out_stats = ee.List.sequence( 0, stations_n_.add( -1 ) ).map( stats_func )
+        return out_stats
+
     out_list = months_seq.map( main_funcs_caller )
-    out_array = ee.Array( out_list ).transpose()
-    out_list_ = out_array.toList()
+
+    def organize_output_func( stationindexobj ):
+        stationIndex = ee.Number( stationindexobj )
+        station_str = ee.String( ee.Feature( stations_.get( stationIndex ) ).get( 'stationID' ) )
+        
+        def stat_iter_func( statindexobj ):
+            statIndex = ee.Number( statindexobj )
+            
+            def month_iter_func( moobj ):
+                mo = ee.Number( moobj )                
+                value = ee.List( ee.List( out_list.get( mo.add( -1 ) ) ).get( stationIndex ) ).get( statIndex )            
+                return value
+
+            out = months_seq.map( month_iter_func )
+            station_stat_str = station_str.cat( '_' ).cat( strs_of_stats_.get( statIndex ) )
+            return ee.Feature( None, {'station_ID': station_stat_str, 'statistic': out} )
     
-    out_fc = ee.FeatureCollection( station_ids_strs_.map( out_list_unpacker ) ).flatten()
-    
+        out = number_of_stats_seq_.map( stat_iter_func )
+        return out
+        
+    out_list = ee.List.sequence( 0, stations_n_.add( -1 ) ).map( organize_output_func )
+    out_list = out_list.flatten()
+    out_fc = ee.FeatureCollection( out_list )
+
     task = ee.batch.Export.table.toDrive( collection=out_fc, 
                                       description='GLDAS_DEMO_2000_2020_{}'.format( str(batch_i) ),
                                       folder='GEE_Downloads',
@@ -155,5 +166,6 @@ for batch_i in range(batch_ct):
         pass
     
     later = dt.datetime.now()
-    print(str(later - now))
-    
+    print(str(later - now))    
+
+
